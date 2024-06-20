b043e7a8fa4b346491e2d7965beb24440fbc98fd
Popinpip42 <luisxl10@gmail.com>
2024-06-20 06:51:39 +0200

    rewriting putnbr optimized

---
+++ putnbr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+void	putnbr(int n)
+{
+	char	buffer[32];
+	int	index = 0;
+
+	if ( n == 0 )
+	{
+		write(1, "0", 2);
+		return;
+	}	
+	if ( n < 0 )
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	while ( n != 0 )
+	{
+		buffer[index++] = ( n % 10 ) + '0';
+		n /= 10; 
+	}
+	//TODO: Revert buffer ahah
+	write(1, &buffer[0], strlen(buffer) + 1);
+}
+
+int	main( void )
+{
+	int	n = -12345;
+	putnbr(n);
+}
+
+
